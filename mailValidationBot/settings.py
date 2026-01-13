676fe664164ef47e6f29abaa9c8d05506b6207c7
mbrsagor <mbrsagor@gmail.com>
2026-01-13 17:14:36 +0600

    New: Added new mail app and base template config

--- mailValidationBot/settings.py
+++ mailValidationBot/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mail',
 ]
 
 MIDDLEWARE = [
