17e2014d2145f13271b7a6e5c54f1672fd1912be
mbrsagor <mbrsagor@gmail.com>
2026-01-28 16:55:21 +0600

    feat: Add Page model with title, slug, content, and publishing status.

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from user.models import Timestamp
 
 
+class Page(Timestamp):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
+
 class Slider(Timestamp):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True, default='')
